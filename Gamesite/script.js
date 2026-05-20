1a7fb524d4f1c28ab5eac394db69c422414b2440
Victor <vitya.lapteff2017@yandex.ru>
2026-05-20 09:07:06 +0300

    JSObject0.2V

---
+++ Gamesite/script.js
@@ -0,0 +1,6 @@
+let name = document.querySelector(`.hero-name`)
+name.textContent = `Alex`
+console.log(name.textContent)
+let clas = document.querySelector(`.hero-class`)
+clas.textContent = `Mage`
+console.log(clas.textContent)
