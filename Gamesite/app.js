64a0a3d52f0fd82530709d3938a1413d7b771af8
Victor <vitya.lapteff2017@yandex.ru>
2026-05-25 18:01:17 +0300

    JSHTML0.3V

--- Gamesite/app.js
+++ Gamesite/app.js
@@ -18,14 +18,28 @@ let btn = document.createElement(`button`)
 btn.textContent = `Добавить задачу`
 btn.classList.add(`button`)
 tsk.append(btn)
-
+let themeChange = document.createElement(`button`)
+themeChange.textContent = `Сменить тему`
+themeChange.classList.add(`button`)
+tsk.append(themeChange)
+let clear = document.createElement(`button`)
+clear.textContent = `Удалить все задачи`
+clear.classList.add(`button`)
+tsk.append(clear)
+let i = 0
+let counter = document.createElement(`div`)
+counter.textContent = `Всего задач: ${i}`
+counter.classList.add(`counter`)
+tsk.append(counter)
 function createTask() {
     let taska = document.createElement(`div`)
     taska.classList.add(`taska`)
     taskList.append(taska)
     let taskaText = document.createElement(`div`)
     taskaText.classList.add(`taskaText`)
-    taskaText.textContent = inp.value
+    // taskaText.textContent = inp.value
+    const result = inp.value.match(/.{1,20}/g).join('\n');
+    taskaText.textContent = result
     taska.append(taskaText)
     let del = document.createElement(`div`)
     del.classList.add(`delete`)
@@ -46,6 +60,8 @@ function createTask() {
     inp.value = null
     del.addEventListener(`click`, function () {
     taska.remove()
+    i--
+    counter.textContent = `Всего задач: ${i}`
     })
     del.addEventListener(`mouseover`, function() {
         del.classList.add(`deleteCheck`)
@@ -56,7 +72,16 @@ function createTask() {
     taska.addEventListener(`click`, function() {
         // функция для смены активности задачи
         taska.classList.toggle(`taskDone`)
+        taskaText.classList.toggle(`taskaTextDone`)
     })
+    clear.addEventListener(`click`, function(){
+        document.querySelectorAll(".taska").forEach(list => {
+        list.remove()})
+        i = 0
+        counter.textContent = `Всего задач: ${i}`
+    })
+    i++
+    counter.textContent = `Всего задач: ${i}`
 }
 btn.addEventListener(`click`, function() {
     if(inp.value == 0) {
@@ -77,8 +102,21 @@ inp.addEventListener(`keyup`, function(e) {
     }   else{}
 })
 
+ themeChange.addEventListener(`click`, function() {
+        if (taskList.classList == `taskList`) {
+            // let list = document.querySelectorAll(`.taska`)
+            // taska.classList.toggle(`taskaDark`)
+            // console.log(list)
+            taskList.classList.replace(`taskList`, `taskListDark`)
+            main.classList.replace(`mainScreen`, `mainScreenDark`)
+        } else {
+            taskList.classList.replace(`taskListDark`, `taskList`)
+            main.classList.replace(`mainScreenDark`, `mainScreen`)
+        }
+})
 // del.addEventListener(`click`, function() {
 //     console.log(MD)
 // })
-
-
+// Для кнопки удалени нужно 
+// 1. найти все элементы на странице в переменной
+// 2. удалить переменную
