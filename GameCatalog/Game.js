9c7c5c3fcf59865e3a430bd9e914d53d9f524441
Victor <vitya.lapeff2017@yandex.ru>
2026-06-09 16:49:20 +0300

    JSFin0.2V

---
+++ GameCatalog/Game.js
@@ -0,0 +1,13 @@
+export class Game {
+    constructor(ID, title, genre, price, platform, rating, year, description, isFavorite) {
+        this.ID = ID;
+        this.title = title;
+        this.genre = genre;
+        this.price = price;
+        this.platform = platform;
+        this.rating = rating;
+        this.year = year;
+        this.description = description;
+        this.isFavorite = isFavorite;
+    }
+}
---
+++ GameCatalog/data.js
@@ -0,0 +1,5 @@
+import { Game } from "./Game.js"
+let gameCards = 0
+let card = new Game(1, "Assasins`creed 2", `Action`, 200 , `PC`, 8.5, 2008, `Почувствой себя настощим скрытным убийцей`, false)
+let card2 = new Game(2, "Sims", `LiveSim`, 1400 , `PC`, 7.3, 2012, `Построй свой город мечты`, true)
+console.log(card, card2)
