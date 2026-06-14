60547c289e32d66025ba286ece63f8a6ef395996
Victor <vitya.lapteff2017@yandex.ru>
2026-06-14 09:55:38 +0300

    React

---
+++ GameCatalog/React/Vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import Card from "./Card.jsx"
+
+function App() {
+console.log(`1,2,3`)
+  return (
+    <>
+      <p className="Count">1,2,3</p> 
+      <Card/>
+      </>
+  )
+}
+
+export default App
---
+++ GameCatalog/React/Vite-project/src/Card.jsx
@@ -0,0 +1,5 @@
+
+function Card() {
+  return <p>4,5,6</p>;
+}
+export default Card;
